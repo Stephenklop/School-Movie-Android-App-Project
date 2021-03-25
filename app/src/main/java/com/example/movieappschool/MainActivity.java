654ac484ee9ba2aae10d166ba9bced5a32030b3a
David van Mourik <dcwvanmourik@gmail.com>
2021-03-25 23:36:23 +0100

    Add API connection In order to connect to an API you will need to add a 'data.properties' (without quotation marks) file inside the root of the project (the 'MovieAppSchool' folder). Then, add the following line inside the 'data.properties' file: 'API_KEY=YOUR-API-KEY-HERE' (again, without quotation marks). That should be all ;)

--- app/src/main/java/com/example/movieappschool/MainActivity.java
+++ app/src/main/java/com/example/movieappschool/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.movieappschool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.movieappschool.domain.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MovieAPITask.MovieListener {
+    private final String API_KEY = BuildConfig.API_KEY;
+    private List<Movie> mMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] params = {"https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY + "&language=en-US&page=1"};
+        new MovieAPITask(this).execute(params);
+    }
+
+    @Override
+    public void setMovies(List<Movie> movies) {
+        // Assign List of movies to movies instance variable
+        mMovies = new ArrayList<>(movies);
+        Log.d("MainActivity", mMovies.toString());
     }
 }
