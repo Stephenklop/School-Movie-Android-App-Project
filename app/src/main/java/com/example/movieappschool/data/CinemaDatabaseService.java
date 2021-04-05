f4b11f2e688a9e3d148ff350f0a40f44db5c7ef4
David van Mourik <dcwvanmourik@gmail.com>
2021-04-05 19:04:26 +0200

    Add CinemaDatabaseService

---
+++ app/src/main/java/com/example/movieappschool/data/CinemaDatabaseService.java
@@ -0,0 +1,61 @@
+package com.example.movieappschool.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CinemaDatabaseService {
+    private String connectionUrl;
+    private String user;
+    private String password;
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    public CinemaDatabaseService(String connectionUrl, String user, String password) {
+        this.connectionUrl = connectionUrl;
+        this.user = user;
+        this.password = password;
+    }
+
+    private void connect() throws SQLException {
+        connection = DriverManager.getConnection(connectionUrl, user, password);
+    }
+
+    private void executeQuery(String query) throws SQLException {
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(query);
+    }
+
+    private void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Integer> getAllMovieIds() {
+        String query = "SELECT * FROM Movies";
+        List<Integer> result = new ArrayList<>();
+
+        try {
+            connect();
+            executeQuery(query);
+
+            while (resultSet.next()) {
+                result.add(resultSet.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            disconnect();
+        }
+
+        return result;
+    }
+}
