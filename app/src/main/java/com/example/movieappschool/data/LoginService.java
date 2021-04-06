dcf4dcdb543560b46195b8200c3069fcf2b03305
Stephen Klop <stefklop18@gmail.com>
2021-04-06 19:25:30 +0200

    Added buttons and fixed id's

--- app/src/main/java/com/example/movieappschool/data/LoginService.java
+++ app/src/main/java/com/example/movieappschool/data/LoginService.java
@@ -15,7 +15,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class LoginService extends AppCompatActivity {
+public class LoginService {
     private CinemaDatabaseService cinemaDatabaseService;
 
     public LoginService() {
