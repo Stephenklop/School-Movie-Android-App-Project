4cc3bcb782c4562a7a8af9197bcc6c9dae6a5fdc
Stephen Klop <stefklop18@gmail.com>
2021-04-07 23:36:23 +0200

    Made changes to the search logic

--- app/src/main/java/com/example/movieappschool/ui/menu/MenuActivity.java
+++ app/src/main/java/com/example/movieappschool/ui/menu/MenuActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
