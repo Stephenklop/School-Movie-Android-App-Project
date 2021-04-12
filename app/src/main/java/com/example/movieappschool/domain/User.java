a029d52ad31423a3c18d25818d015b53f98d67b6
Erwin Jobse <erwin@jobse.org>
2021-04-12 17:17:47 +0200

    user validation
    todo: check if username is in use

--- app/src/main/java/com/example/movieappschool/domain/User.java
+++ app/src/main/java/com/example/movieappschool/domain/User.java
@@ -6,52 +6,15 @@ public class User {
     private int mUserId;
 
     public User(int mUserId, String mFirstName, String mLastName, String mUsername, String mAddress, String mEmail, String mPassword, String mDateBirth) {
-        if (mFirstName != null) {
-            this.mFirstName = mFirstName;
-        } else {
-            throw new IllegalArgumentException("Firstname is invalid");
-        }
-
-        if (mLastName != null) {
-            this.mLastName = mLastName;
-        } else {
-            throw new IllegalArgumentException("Firstname is invalid");
-        }
-
-        if (mUsername != null) {
-            this.mUsername = mUsername;
-        } else {
-            throw new IllegalArgumentException("Username is invalid");
-        }
-
-        if (mAddress != null) {
-            this.mAddress = mAddress;
-        } else {
-            throw new IllegalArgumentException("Username is invalid");
-        }
-        //email must be like user@domain.tld
-        if (mEmail != null & mEmail.matches("^(.+)@(.+)$")) {
-            this.mEmail = mEmail;
-        } else {
-            throw new IllegalArgumentException("email is invalid");
-        }
-        //mPassword must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
-        if (mPassword != null & mPassword.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$") ) {
-            this.mPassword = mPassword;
-        } else {
-            throw new IllegalArgumentException("password is invalid");
-        }
-
-        if (mUserId > 0) {
-            this.mUserId = mUserId;
-        } else {
-            throw new IllegalArgumentException("UserId is invalid");
-        }
-        //mDateBirth is allowed to be dd/mm/yyyy
-        if (mDateBirth != null & mDateBirth.matches("(^(((0[1-9]|1[0-9]|2[0-8])[\\/](0[1-9]|1[012]))|((29|30|31)[\\/](0[13578]|1[02]))|((29|30)[\\/](0[4,6,9]|11)))[\\/](19|[2-9][0-9])\\d\\d$)|(^29[\\/]02[\\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)\n")) {
-            this.mDateBirth = mDateBirth;
-            throw new IllegalArgumentException("Datebirth is invalid");
-        }
+        this.mFirstName = mFirstName;
+        this.mLastName = mLastName;
+        this.mUsername = mUsername;
+        this.mAddress = mAddress;
+        this.mEmail = mEmail;
+        this.mPassword = mPassword;
+        this.mUserId = mUserId;
+        this.mDateBirth = mDateBirth;
+            
     }
 
     public int getUserId() {
--- app/src/main/java/com/example/movieappschool/ui/RegisterActivity.java
+++ app/src/main/java/com/example/movieappschool/ui/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,6 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
         mDateBirth = findViewById(R.id.register_birthdate_input);
         mCreateAccount = findViewById(R.id.register_submit_button);
 
+
         mCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,7 +52,9 @@ public class RegisterActivity extends AppCompatActivity {
                 String address = mAddress.getText().toString();
                 String datebirth = mDateBirth.getText().toString();
 
-                if(checkIfFilled(username, firstname, lastname, password, email, address, datebirth)) {
+
+
+                if(validate()) {
                     Thread t1 = new Thread(new Runnable() {
                         @Override
                         public void run(){
@@ -77,12 +81,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    public boolean checkIfFilled(String username, String firstname, String lastname, String password, String email, String address, String datebirth) {
-        if (!username.isEmpty() && !firstname.isEmpty() && !lastname.isEmpty() && !password.isEmpty() && !email.isEmpty() && !address.isEmpty() && !datebirth.isEmpty()) {
-            return true;
-        }
-        return false;
-    }
+
 
     private String hashPassword(String password) {
         String hashedPassword = "";
@@ -101,4 +100,56 @@ public class RegisterActivity extends AppCompatActivity {
         }
         return hashedPassword;
     }
+
+    private boolean validate(){
+        boolean valid = true;
+
+        if (mUsername.getText().toString().isEmpty()){
+            mUsername.setError("controleer gebruikersnaam");
+            valid = false;
+        }
+
+        if (mFirstname.getText().toString().isEmpty()){
+            mFirstname.setError("controleer voornaam");
+            valid = false;
+        }
+
+        if (mLastname.getText().toString().isEmpty()){
+            mLastname.setError("controleer achternaam");
+            valid = false;
+        }
+
+        //email must be like user@domain.tld
+        if (!mEmail.getText().toString().matches("^(.+)@(.+)$")){
+            mEmail.setError("controleer email");
+            valid = false;
+        }
+        //mPassword must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+        if (!mPassword.getText().toString().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")){
+            mPassword.setError("Wachtwoord moet minimaal uit 8 tekens bestaan waarvan 1 hoofdletter, 1 special character en 1 cijfer");
+            valid = false;
+        }
+
+        //mDateBirth is allowed to be dd/mm/yyyy
+        if (!mDateBirth.getText().toString().matches("(^(((0[1-9]|1[0-9]|2[0-8])[\\/](0[1-9]|1[012]))|((29|30|31)[\\/](0[13578]|1[02]))|((29|30)[\\/](0[4,6,9]|11)))[\\/](19|[2-9][0-9])\\d\\d$)|(^29[\\/]02[\\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)\n")){
+            mDateBirth.setError("Formaat geboortedatum moeten 01/01/2000 zijn");
+            valid = false;
+        }
+
+        if (mAddress.getText().toString().isEmpty()){
+            mAddress.setError("controleer Adres");
+            valid = false;
+        }
+
+        if (!valid){
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+
+
+
 }
