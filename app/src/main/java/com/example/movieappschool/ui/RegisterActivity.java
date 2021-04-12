b0c51ea8c1ad58fe288d4b7afa41e4013a339a9b
Erwin Jobse <erwin@jobse.org>
2021-04-12 17:25:13 +0200

    compatible with date picker

--- app/src/main/java/com/example/movieappschool/ui/RegisterActivity.java
+++ app/src/main/java/com/example/movieappschool/ui/RegisterActivity.java
@@ -85,11 +85,10 @@ public class RegisterActivity extends AppCompatActivity {
                 String datebirth = mDateBirth.getText().toString();
 
 
-
-                if(validate()) {
+                if (validate()) {
                     Thread t1 = new Thread(new Runnable() {
                         @Override
-                        public void run(){
+                        public void run() {
                             String hashedPassword = hashPassword(password);
 
                             System.out.println("Hashed password: " + hashedPassword);
@@ -124,7 +123,7 @@ public class RegisterActivity extends AppCompatActivity {
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                mDateBirth.setText(dayOfMonth + "-" + (month+1) + "-" + year);
+                mDateBirth.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
             }
         }, mYear, mMonth, mDay);
         datePickerDialog.show();
@@ -156,47 +155,47 @@ public class RegisterActivity extends AppCompatActivity {
         return hashedPassword;
     }
 
-    private boolean validate(){
+    private boolean validate() {
         boolean valid = true;
 
-        if (mUsername.getText().toString().isEmpty()){
-            mUsername.setError("controleer gebruikersnaam");
+        if (mUsername.getText().toString().isEmpty()) {
+            mUsername.setError("Controleer gebruikersnaam");
             valid = false;
         }
 
-        if (mFirstname.getText().toString().isEmpty()){
-            mFirstname.setError("controleer voornaam");
+        if (mFirstname.getText().toString().isEmpty()) {
+            mFirstname.setError("Controleer voornaam");
             valid = false;
         }
 
-        if (mLastname.getText().toString().isEmpty()){
-            mLastname.setError("controleer achternaam");
+        if (mLastname.getText().toString().isEmpty()) {
+            mLastname.setError("Controleer achternaam");
             valid = false;
         }
 
         //email must be like user@domain.tld
-        if (!mEmail.getText().toString().matches("^(.+)@(.+)$")){
-            mEmail.setError("controleer email");
+        if (!mEmail.getText().toString().matches("^(.+)@(.+)$")) {
+            mEmail.setError("Controleer email");
             valid = false;
         }
         //mPassword must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
-        if (!mPassword.getText().toString().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")){
+        if (!mPassword.getText().toString().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")) {
             mPassword.setError("Wachtwoord moet minimaal uit 8 tekens bestaan waarvan 1 hoofdletter, 1 special character en 1 cijfer");
             valid = false;
         }
 
         //mDateBirth is allowed to be dd/mm/yyyy
-        if (!mDateBirth.getText().toString().matches("(^(((0[1-9]|1[0-9]|2[0-8])[\\/](0[1-9]|1[012]))|((29|30|31)[\\/](0[13578]|1[02]))|((29|30)[\\/](0[4,6,9]|11)))[\\/](19|[2-9][0-9])\\d\\d$)|(^29[\\/]02[\\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)\n")){
-            mDateBirth.setError("Formaat geboortedatum moeten 01/01/2000 zijn");
+        if (mDateBirth.getText().toString().isEmpty()) {
+            mDateBirth.setError("Controlleer geboortedatum");
             valid = false;
         }
 
-        if (mAddress.getText().toString().isEmpty()){
-            mAddress.setError("controleer Adres");
+        if (mAddress.getText().toString().isEmpty()) {
+            mAddress.setError("Controleer Adres");
             valid = false;
         }
 
-        if (!valid){
+        if (!valid) {
             return false;
         } else {
             return true;
@@ -205,17 +204,14 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-
-
-
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
             View v = getCurrentFocus();
-            if(v instanceof EditText) {
+            if (v instanceof EditText) {
                 Rect outRect = new Rect();
                 v.getGlobalVisibleRect(outRect);
-                if(!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
                     v.clearFocus();
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
