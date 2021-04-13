48e9409ceab6108f344b39b3de32d1afe35930c8
Stephen <hi@stephenklop.nl>
2021-04-13 11:06:20 +0200

    Fixed change password page ui

--- app/src/main/java/com/example/movieappschool/ui/PasswordActivity.java
+++ app/src/main/java/com/example/movieappschool/ui/PasswordActivity.java
@@ -4,9 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -48,21 +51,18 @@ public class PasswordActivity extends AppCompatActivity {
         Intent intent = getIntent();
         previousActivity = intent.getStringExtra("prevActivity");
 
+        System.out.println("Prev: " + previousActivity);
+
         toolbar = findViewById(R.id.change_password_toolbar);
         toolbar.findViewById(R.id.hamburger_icon).setVisibility(View.INVISIBLE);
 
         backButton = toolbar.findViewById(R.id.back_icon);
         backButton.setVisibility(View.VISIBLE);
         backButton.setOnClickListener(v -> {
-            try {
-                Intent backIntent = new Intent(getApplicationContext(), Class.forName(previousActivity));
-                backIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            Intent backIntent = new Intent(getApplicationContext(), AccountActivity.class);
+            backIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                getApplicationContext().startActivity(backIntent);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-                e.getMessage();
-            }
+            getApplicationContext().startActivity(backIntent);
         });
 
         mChangePasswordButton = findViewById(R.id.change_password_submit);
@@ -138,5 +138,22 @@ public class PasswordActivity extends AppCompatActivity {
                 startActivity(login);
             }
         }, intentFilter);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if(v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if(!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
